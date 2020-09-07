a63f862b21d2307c4a9352fef6151734715ca4ab
concrode <captainshenhua@gmail.com>
2020-09-07 20:59:30 +1000

    Updates

--- src/array/LengthOfLongestFibonacciSubsequence.java
+++ src/array/LengthOfLongestFibonacciSubsequence.java
@@ -1,4 +1,74 @@
 package array;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A sequence X_1, X_2, ..., X_n is fibonacci-like if:
+ *
+ * n >= 3
+ * X_i + X_{i+1} = X_{i+2} for all i + 2 <= n
+ * Given a strictly increasing array A of positive integers forming a sequence,
+ * find the length of the longest fibonacci-like subsequence of A.  If one does not exist, return 0.
+ *
+ * (Recall that a subsequence is derived from another sequence A by deleting any number of elements (including none) from A,
+ * without changing the order of the remaining elements.  For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].)
+ *
+ * Example 1:
+ * Input: [1,2,3,4,5,6,7,8]
+ * Output: 5
+ * Explanation:
+ * The longest subsequence that is fibonacci-like: [1,2,3,5,8].
+ *
+ * Example 2:
+ * Input: [1,3,7,11,12,14,18]
+ * Output: 3
+ * Explanation:
+ * The longest subsequence that is fibonacci-like:
+ * [1,11,12], [3,11,14] or [7,11,18].
+ *
+ * Note:
+ * 3 <= A.length <= 1000
+ * 1 <= A[0] < A[1] < ... < A[A.length - 1] <= 10^9
+ * (The time limit has been reduced by 50% for submissions in Java, C, and C++.)
+ *
+ */
 public class LengthOfLongestFibonacciSubsequence {
+
+    /**
+     * Algorithm
+     * For each starting pair A[i], A[j], we maintain the next expected value y = A[i] + A[j] and the previously
+     * seen largest value x = A[j]. If y is in the array, then we can then update these values (x, y) -> (y, x+y).
+     *
+     * Also, because subsequences are only fibonacci-like if they have length 3 or more, we must perform the
+     * check ans >= 3 ? ans : 0 at the end.
+     *
+     * @param A
+     * @return
+     */
+    public int lenLongestFibSubseq(int[] A) {
+        int N = A.length;
+        Set<Integer> S = new HashSet();
+        for (int x: A) S.add(x);
+
+        int ans = 0;
+        for (int i = 0; i < N; ++i)
+            for (int j = i+1; j < N; ++j) {
+                /* With the starting pair (A[i], A[j]),
+                 * y represents the future expected value in
+                 * the fibonacci subsequence, and x represents
+                 * the most current value found. */
+                int x = A[j], y = A[i] + A[j];
+                int length = 2;
+                while (S.contains(y)) {
+                    // x, y -> y, x+y
+                    int tmp = y;
+                    y += x;
+                    x = tmp;
+                    ans = Math.max(ans, ++length);
+                }
+            }
+
+        return ans >= 3 ? ans : 0;
+    }
 }
